3a1a8b320d1422239858ba6a7718406b112512c1
uingxx <feng.xingxu@gmail.com>
2017-01-09 16:58:19 -0800

    added utilities header

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// rats.cpp
+// rats.cpp==
 
 #include <iostream>
 #include <string>
@@ -6,27 +6,9 @@
 #include <utility>
 #include <cstdlib>
 #include <cctype>
+#include "utilities.h"
 using namespace std;
 
-///////////////////////////////////////////////////////////////////////////
-// Manifest constants
-///////////////////////////////////////////////////////////////////////////
-
-const int MAXROWS = 20;            // max number of rows in the arena
-const int MAXCOLS = 20;            // max number of columns in the arena
-const int MAXRATS = 100;           // max number of rats allowed
-const int INITIAL_RAT_HEALTH = 2;  // initial rat health
-const int POISONED_IDLE_TIME = 1;  // poisoned rat idles this many turns
-								   //   between moves
-
-const int NORTH = 0;
-const int EAST = 1;
-const int SOUTH = 2;
-const int WEST = 3;
-const int NUMDIRS = 4;
-
-const int EMPTY = 0;
-const int HAS_POISON = 1;
 
 ///////////////////////////////////////////////////////////////////////////
 // Type definitions
