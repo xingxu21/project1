81ad48764674f1914f809aaad1350213611ca694
uingxx <feng.xingxu@gmail.com>
2017-01-09 17:13:01 -0800

    rat header

--- main.cpp
+++ main.cpp
@@ -7,37 +7,10 @@
 #include <cstdlib>
 #include <cctype>
 #include "utilities.h"
+#include "Rat.h"
 using namespace std;
 
 
-///////////////////////////////////////////////////////////////////////////
-// Type definitions
-///////////////////////////////////////////////////////////////////////////
-
-class Arena;  // This is needed to let the compiler know that Arena is a
-			  // type name, since it's mentioned in the Rat declaration.
-
-class Rat
-{
-public:
-	// Constructor
-	Rat(Arena* ap, int r, int c);
-
-	// Accessors
-	int  row() const;
-	int  col() const;
-	bool isDead() const;
-
-	// Mutators
-	void move();
-
-private:
-	Arena* m_arena;
-	int    m_row;
-	int    m_col;
-	int    m_health;
-	int    m_idleTurnsRemaining;
-};
 
 class Player
 {
